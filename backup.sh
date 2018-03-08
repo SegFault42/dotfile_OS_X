8de7f4b9ed729a6d6d22b7c0fa3be41499109bc7
SegFault42 <rabougue@student.42.fr>
2018-03-08 17:26:54 +0100

    1st commit

---
+++ backup.sh
@@ -0,0 +1,8 @@
+mkdir chunkwm
+mkdir ncmpcpp
+
+cp -fr ~/.chunkwmrc ./chunkwm/
+cp -fr ~/.khdrc ./chunkwm/
+cp -fr ~/Library/Preferences/kitty/kitty.conf .
+cp -fr ~/.zshrc .
+cp -fr ~/.mpd/ ./ncmpcpp/
